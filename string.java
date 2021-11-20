a95b375e55a5898d3ab6e9ca6984e3e8e41dfc37
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2021-11-20 16:49:39 +0530

    string

--- string.java
+++ string.java
@@ -1,21 +1,27 @@
 import java.util.*;
-import java.security.*;
-public class string {
 
- public static void main(String[] args) {
-try {
-   Scanner in = new Scanner(System.in);
-   int n = in .nextInt();
-   in.close();
-String s = Integer.toString(n);
-if (n == Integer.parseInt(s)) {
-    System.out.println("Good job");
-   } else {
-    System.out.println("Wrong answer.");
-   }
+public class string 
+{
+
+ public static void main(String[] args) 
    {
-   System.out.println("Unsuccessful Termination!!");
+      Scanner in = new Scanner(System.in);
+      int n = in .nextInt();
+      in.close();
+      String s = Integer.toString(n);
+      if (n == Integer.parseInt(s)) 
+         {
+         System.out.println("Good job");
+         } 
+      else 
+         {
+         System.out.println("Wrong answer.");
+         }
    }
+}
+
+
+
 
  
   
