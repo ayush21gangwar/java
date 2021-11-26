de03e34518142fbc8c231a4a7ba31a10421fffd5
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2021-11-27 00:37:32 +0530

    functions

---
+++ functions.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class functions {
+    public static int sumoftwonumbers(int a,int b){
+        int sum;
+        sum=a+b;
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        int c;
+        sumoftwonumbers(a,b);
+        c=a+b;
+        System.out.println(c);
+        //System.out.println("sum of two numbers is " + sum);
+
+
+    }
+}
+
