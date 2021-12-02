2b2d155c238d1004cd72a010a53ad57fc5a71689
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2021-12-03 00:15:08 +0530

    while loop

---
+++ whileloop.java
@@ -0,0 +1,14 @@
+
+public class whileloop {
+    public static void main(String[] args) {
+        
+    
+      //  while (a<5);
+        int a=1;
+        while(a<10){
+            System.out.println(a);
+            a++;
+        }
+    }
+}
+
