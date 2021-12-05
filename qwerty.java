4b65ff900f386a5920142674a576fc12765883d5
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2021-12-05 20:01:24 +0530

    qwerty

---
+++ qwerty.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class qwerty {
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+        int b = a.nextInt();
+        int i = 1;
+        while (i <=b) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+
