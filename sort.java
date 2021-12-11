063254d78fa22b84b1ab0ed01b22fc2718583dfd
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2021-12-11 21:37:44 +0530

    sort

---
+++ sort.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class sort {
+    public static void main(String[] args) {
+        Scanner src= new Scanner(System.in);
+        int a[]={1,2,3,4,5};
+        int b[]={6,7,8,9,10};
+        int length_of_first_array=a.length;
+        int length_of_second_array=b.length;
+        int result=  length_of_first_array+length_of_second_array;
+        System.out.println(result);
+    }
+}
+
