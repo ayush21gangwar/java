522d0de5c9fdcee914be51decdc889038cf02d7c
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2021-12-12 18:43:30 +0530

    prblm_2

---
+++ prblm_2.java
@@ -0,0 +1,14 @@
+public class prblm_2 {
+    public static void main(String[] args) {
+        System.out.println("enter the number");
+        Scanner src= new Scanner(System.in);
+        int a= src.nextInt();
+        if (a % 2==0)
+        System.out.println("entered number is even");
+        else
+            System.out.println("entered number is odd");
+
+    }
+
+    }
+
