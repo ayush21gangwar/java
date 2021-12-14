23475ca730abe88abab7b13d6df540e3923c13bb
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2021-12-14 21:00:23 +0530

    asdf

---
+++ pre_and_post_increment.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class pre_and_post_increment {
+    public static void main(String[] args) {
+        Scanner src= new Scanner(System.in);
+        System.out.println("enter the value of a");
+        int a = src.nextInt();
+        System.out.println("enter the value of b");
+        int b= src.nextInt();
+        System.out.println(a>>2);
+        System.out.println(b<<2);
+    }
+}
+
