13a2fd85f1941982807e8bf7317fec0ea02a7a07
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2022-02-26 21:45:53 +0530

    max_min_arr

---
+++ max_min_arr.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class max_min_arr {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int arr[]={45,65,78,99,12,34,56,98};
+        Arrays.sort(arr);
+
+        System.out.println("minimum" +arr[0]);
+        System.out.println("maximum" +arr[arr.length-1]);
+    }
+}
