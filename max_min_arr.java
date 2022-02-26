fa98405dd378352534e5f6fe25f8b70cb59963f1
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2022-02-26 21:47:59 +0530

    max_min_arr

--- max_min_arr.java
+++ max_min_arr.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class max_min_arr {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+        //Scanner sc= new Scanner(System.in);
         int arr[]={45,65,78,99,12,34,56,98};
         Arrays.sort(arr);
 
