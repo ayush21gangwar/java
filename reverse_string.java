f36e6bd06f8722f8d2d08c157c74a82d9f81eea0
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2022-02-27 21:39:34 +0530

    reverse_string

---
+++ reverse_string.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class reverse_string {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+       // String name= "ayush";
+        StringBuilder sb=new StringBuilder("ayush");
+        sb.reverse();
+        //return sb.toString();
+        System.out.println(sb);
+    }
+}
+
